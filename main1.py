759be3cc56c6f267142d7de2b284709457e029d5
Gunther <62240649+Gunthersuper@users.noreply.github.com>
2024-02-03 22:33:44 +0400

    Update main1.py

--- main1.py
+++ main1.py
@@ -10,10 +10,10 @@ client = UMFutures(key = api, secret=secret)
 # 0.012 means +1.2%, 0.009 is -0.9%
 tp = 0.012
 sl = 0.009
-volume = 500
+volume = 10  # volume for one order (if its 10 and leverage is 10, then you put 1 usdt to one position)
 leverage = 10
-type = 'ISOLATED'
-# type is 'ISOLATED' or 'CROSS'
+type = 'ISOLATED'  # type is 'ISOLATED' or 'CROSS'
+qty = 100  # Amount of concurrent opened positions
 
 # getting your futures balance in USDT
 def get_balance_usdt():
@@ -30,8 +30,6 @@ def get_balance_usdt():
             )
         )
 
-print("My balance is: ", get_balance_usdt(), " USDT")
-
 
 # Getting all available symbols on the Futures ('BTCUSDT', 'ETHUSDT', ....)
 def get_tickers_usdt():
@@ -46,7 +44,7 @@ def get_tickers_usdt():
 # Getting candles for the needed symbol, its a dataframe with 'Time', 'Open', 'High', 'Low', 'Close', 'Volume'
 def klines(symbol):
     try:
-        resp = pd.DataFrame(client.klines(symbol, '1h'))
+        resp = pd.DataFrame(client.klines(symbol, '15m'))
         resp = resp.iloc[:,:6]
         resp.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
         resp = resp.set_index('Time')
@@ -171,6 +169,34 @@ def check_positions():
             )
         )
 
+def get_pos():
+    try:
+        resp = client.get_position_risk()
+        pos = []
+        for elem in resp:
+            if float(elem['positionAmt']) != 0:
+                pos.append(elem['symbol'])
+        return pos
+    except ClientError as error:
+        print(
+            "Found error. status: {}, error code: {}, error message: {}".format(
+                error.status_code, error.error_code, error.error_message
+            )
+        )
+
+def check_orders():
+    try:
+        response = client.get_orders(recvWindow=6000)
+        sym = []
+        for elem in response:
+            sym.append(elem['symbol'])
+        return sym
+    except ClientError as error:
+        print(
+            "Found error. status: {}, error code: {}, error message: {}".format(
+                error.status_code, error.error_code, error.error_message
+            )
+        )
 
 # Close open orders for the needed symbol. If one stop order is executed and another one is still there
 def close_open_orders(symbol):
@@ -184,59 +210,149 @@ def close_open_orders(symbol):
             )
         )
 
-# Strategy. Can use any other:
-def check_macd_ema(symbol):
-    kl = klines(symbol)
-    if ta.trend.macd_diff(kl.Close).iloc[-1] > 0 and ta.trend.macd_diff(kl.Close).iloc[-2] < 0 \
-    and ta.trend.ema_indicator(kl.Close, window=200).iloc[-1] < kl.Close.iloc[-1]:
-        return 'up'
 
-    elif ta.trend.macd_diff(kl.Close).iloc[-1] < 0 and ta.trend.macd_diff(kl.Close).iloc[-2] > 0 \
-    and ta.trend.ema_indicator(kl.Close, window=200).iloc[-1] > kl.Close.iloc[-1]:
+def check_last_order_pnl():
+    try:
+        response = client.get_income_history(recvWindow=6000)
+        pnl = []
+        time = 0
+        inc = ''
+        for elem in response:
+            if elem['incomeType'] == 'REALIZED_PNL':
+                pnl.append(elem)
+        for elem in pnl:
+            if float(elem['time']) > time:
+                inc = float(elem['income'])
+        return inc
+    except ClientError as error:
+        print(
+            "Found error. status: {}, error code: {}, error message: {}".format(
+                error.status_code, error.error_code, error.error_message
+            )
+        )
+
+# Strategy. Can use any other:
+def str_signal(symbol):
+    kl = klines(symbol)
+    rsi = ta.momentum.RSIIndicator(kl.Close).rsi()
+    rsi_k = ta.momentum.StochRSIIndicator(kl.Close).stochrsi_k()
+    rsi_d = ta.momentum.StochRSIIndicator(kl.Close).stochrsi_d()
+    ema = ta.trend.ema_indicator(kl.Close, window=200)
+    if rsi.iloc[-1] < 40 and ema.iloc[-1] < kl.Close.iloc[-1] and rsi_k.iloc[-1] < 20 and rsi_k.iloc[-3] < rsi_d.iloc[-3] and rsi_k.iloc[-2] < rsi_d.iloc[-2] and rsi_k.iloc[-1] > rsi_d.iloc[-1]:
+        return 'up'
+    if rsi.iloc[-1] > 60 and ema.iloc[-1] > kl.Close.iloc[-1] and rsi_k.iloc[-1] > 80 and rsi_k.iloc[-3] > rsi_d.iloc[-3] and rsi_k.iloc[-2] > rsi_d.iloc[-2] and rsi_k.iloc[-1] < rsi_d.iloc[-1]:
         return 'down'
 
     else:
         return 'none'
 
 
-order = False
+def rsi_signal(symbol):
+    kl = klines(symbol)
+    rsi = ta.momentum.RSIIndicator(kl.Close).rsi()
+    ema = ta.trend.ema_indicator(kl.Close, window=200)
+    if rsi.iloc[-2] < 30 and rsi.iloc[-1] > 30:
+        return 'up'
+    if rsi.iloc[-2] > 70 and rsi.iloc[-1] < 70:
+        return 'down'
+
+    else:
+        return 'none'
+
+
+def macd_ema(symbol):
+    kl = klines(symbol)
+    macd = ta.trend.macd_diff(kl.Close)
+    ema = ta.trend.ema_indicator(kl.Close, window=200)
+    if macd.iloc[-3] < 0 and macd.iloc[-2] < 0 and macd.iloc[-1] > 0 and ema.iloc[-1] < kl.Close.iloc[-1]:
+        return 'up'
+    if macd.iloc[-3] > 0 and macd.iloc[-2] > 0 and macd.iloc[-1] < 0 and ema.iloc[-1] > kl.Close.iloc[-1]:
+        return 'down'
+    else:
+        return 'none'
+
+
+def ema200_50(symbol):
+    kl = klines(symbol)
+    ema200 = ta.trend.ema_indicator(kl.Close, window=100)
+    ema50 = ta.trend.ema_indicator(kl.Close, window=50)
+    if ema50.iloc[-3] < ema200.iloc[-3] and ema50.iloc[-2] < ema200.iloc[-2] and ema50.iloc[-1] > ema200.iloc[-1]:
+        return 'up'
+    if ema50.iloc[-3] > ema200.iloc[-3] and ema50.iloc[-2] > ema200.iloc[-2] and ema50.iloc[-1] < ema200.iloc[-1]:
+        return 'down'
+    else:
+        return 'none'
+
+
+orders = 0
 symbol = ''
+# getting all symbols from Binace Futures list:
 symbols = get_tickers_usdt()
 
 while True:
-    positions = check_positions()
-    print(f'You have {positions} opened positions')
-    if positions == 0:
-        order = False
-        if symbol != '':
-            close_open_orders(symbol)
-    else:
-        order = True
+    # we need to get balance to check if the connection is good, or you have all the needed permissions
+    balance = get_balance_usdt()
+    sleep(1)
+    if balance == None:
+        print('Cant connect to API. Check IP, restrictions or wait some time')
+    if balance != None:
+        print("My balance is: ", balance, " USDT")
+        # getting position list:
+        pos = []
+        pos = get_pos()
+        print(f'You have {len(pos)} opened positions:\n{pos}')
+        # Getting order list
+        ord = []
+        ord = check_orders()
+        # removing stop orders for closed positions
+        for elem in ord:
+            if not elem in pos:
+                close_open_orders(elem)
 
-    if order == False:
-        for elem in symbols:
-            signal = check_macd_ema(elem)
-            if signal == 'up' and symbol != 'USDCUSDT':
-                print('Found BUY signal for ', elem)
-                set_mode(elem, type)
-                sleep(1)
-                set_leverage(elem, leverage)
-                sleep(1)
-                print('Placing order for ', elem)
-                open_order(elem, 'buy')
-                symbol = elem
-                order = True
-                break
-            if signal == 'down' and symbol != 'USDCUSDT':
-                print('Found SELL signal for ', elem)
-                set_mode(elem, type)
-                sleep(1)
-                set_leverage(elem, leverage)
-                sleep(1)
-                print('Placing order for ', elem)
-                open_order(elem, 'sell')
-                symbol = elem
-                order = True
-                break
-    print('Waiting 5 min')
-    sleep(300)
+        if len(pos) < qty:
+            for elem in symbols:
+                # Strategies (you can make your own with the TA library):
+
+                # signal = str_signal(elem)
+                signal = rsi_signal(elem)
+                # signal = macd_ema(elem)
+
+                # 'up' or 'down' signal, we place orders for symbols that arent in the opened positions and orders
+                # we also dont need USDTUSDC because its 1:1 (dont need to spend money for the commission)
+                if signal == 'up' and elem != 'USDCUSDT' and not elem in pos and not elem in ord and elem != symbol:
+                    print('Found BUY signal for ', elem)
+                    set_mode(elem, type)
+                    sleep(1)
+                    set_leverage(elem, leverage)
+                    sleep(1)
+                    print('Placing order for ', elem)
+                    open_order(elem, 'buy')
+                    symbol = elem
+                    order = True
+                    pos = get_pos()
+                    sleep(1)
+                    ord = check_orders()
+                    sleep(1)
+                    sleep(10)
+                    # break
+                if signal == 'down' and elem != 'USDCUSDT' and not elem in pos and not elem in ord and elem != symbol:
+                    print('Found SELL signal for ', elem)
+                    set_mode(elem, type)
+                    sleep(1)
+                    set_leverage(elem, leverage)
+                    sleep(1)
+                    print('Placing order for ', elem)
+                    open_order(elem, 'sell')
+                    symbol = elem
+                    order = True
+                    pos = get_pos()
+                    sleep(1)
+                    ord = check_orders()
+                    sleep(1)
+                    sleep(10)
+                    # break
+    print('Waiting 3 min')
+    sleep(180)
+
+
+
